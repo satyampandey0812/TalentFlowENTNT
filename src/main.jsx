5f8a2198f08a595494be02f10ed975e058447090
satyam kumar <satyamkumar1500000@gmail.com>
2025-09-20 18:42:06 +0530

    get

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,17 @@
-// main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { makeServer } from "./Server";
 
-// Check if Mirage is running
-console.log('Setting up MirageJS server...');
-
-if (process.env.NODE_ENV === "development") {
- const server = makeServer({ environment: "development" });
-  console.log('MirageJS server created:', server);
-  
-  // Test if MirageJS is intercepting requests
-  fetch('/api/jobs?page=1&pageSize=2')
-    .then(res => res.json())
-    .then(data => console.log('Mirage test response:', data))
-    .catch(err => console.error('Mirage test failed:', err));
-}
+// Always start MirageJS, even in production (for Vercel demo)
+makeServer({ environment: "production" });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
